b304ce5eccec75cfa56b5f66e17b812f6fae1564
Seong-hun Kim <kshoon92@gmail.com>
2020-06-17 11:57:39 +0900

    wip

--- main.py
+++ main.py
@@ -107,7 +107,13 @@ class ActorCritic(BaseEnv):
         # e = e / (np.abs(e) + EPS)
         self.wc.dot = - config.ETA * dphicf * e
         self.wt.dot = - config.ETA * (-2) * (prpf.dot(wa) - pruf) * e
-        self.wa.dot = - 1 * (wa - wt) * np.exp(- 10 * np.abs(e))
+        # self.wa.dot = - 1 * (wa - wt) * np.exp(- 10 * np.abs(e))
+        self.wa.dot = (
+            - 1 * (wa - wt)
+            + config.ETA * prpf.dot(wa) * (
+                wc.T.dot(dphicf) - 2 * wt.T.dot(prpf.dot(wa) - pruf)
+            )
+        )
 
     def get_error(self, x, filter_state, ac_state):
         qf, phicf, prpf, pruf = filter_state
